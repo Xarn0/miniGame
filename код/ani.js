ca4f2bd5e3c6ad2d292d7f752add5b34d92df271
Xouli <xouli@list.ru>
2020-09-10 09:09:35 +0700

    Игра

---
+++ код/ani.js
@@ -0,0 +1,472 @@
+// const react = document.querySelector('.react');
+
+
+// document.querySelector('.btn').addEventListener('click',()=>{
+//     let post = 0;
+//     let topp = 250;
+//     let tt = setInterval(pp,20);
+// let str = 'вниз'
+//     function pp (){
+        
+      
+//         if(str === 'вниз'){
+//             post++
+//             react.style.offset.top = post + 'px';
+//             if(post > 200){
+//                 post = 200;
+//                 str = 'вв';
+//                            }
+//         else if (str === 'вв'){
+            
+//             post++;
+//             react.style.right = post + 'px';
+//             if(post == 200){
+//                 console.log('dd')
+//                 ;}
+//                            }
+//                        }}
+//     })
+//     let i= {
+        
+//                 pd:[2,3,4],
+         
+//         pr: 33
+//     }
+//     console.log(i.pd.length)
+//         // if(post == 250)
+//         // {
+//         //     // 
+//         //     topp--;
+//         //     react.style.top  = topp + 'px';
+//         //     react.style.left = topp + 'px';
+//         //     // if(post==0){
+//         //     //     clearTimeour(tt)
+//         //     // }
+//         // }
+//         // else{
+//         // post++;
+//         // react.style.top  = post + 'px'
+        
+
+        
+    //     if('v')
+// let re= document.querySelectorAll('.p').forEach(i=>{
+//     i.addEventListener('click',(e)=>{
+    
+//     if(e.target.tagName === 'IMG'){
+//       e.target.classList.toggle('t1');
+//        i.addEventListener('blur',(e)=>{
+          
+//         e.target.classList.toggle('t2');
+//        },flase)
+       
+       
+//     }
+// })
+
+// })
+// let rec = document.querySelectorAll('.react');
+//  rec.addEventListener('keypress',(e)=>{
+//      console.log(e);
+    
+//     //     rec[2].classList.('react')
+//     //   rec[2].classList.add('t1');
+      
+//     })
+ 
+
+
+
+
+
+
+// // .addEventListener('keydown',(e)=>{
+   
+// //         console.log(e.code)
+    
+// // })
+
+// let left = 0;
+// let top1 = 0;
+
+// document.onkeypress = function (e){
+//     console.log(e.keyCode);
+//     let b11 =document.querySelector('.b11');
+//     let play = document.querySelector('.react');
+//      if(e.keyCode===100){
+//         left= left + 10;
+//         play.style.marginLeft = left + 'px';
+//         console.log('22d')
+//     }
+//     else if(e.keyCode===97){
+//         left= left - 10;
+//     play.style.marginLeft = left + 'px';
+//     console.log('22d')}
+
+//     else if(e.keyCode===115){
+//         top1= top1+ 10;
+//     play.style.marginTop = top1 + 'px';
+//     console.log('22d')}
+
+//     else if(e.keyCode===119){
+//         top1= top1- 10;
+//     play.style.marginTop = top1 + 'px';
+   
+//     console.log('22d')};
+//       if(play==b11){
+//         play.style.marginTop ='200px';
+//         play.style.marginLeft = "200px";
+// }
+// document.querySelector('.rr').onkeypress = (e)=>{(console.log(e.keyCodess))}
+
+
+// }
+
+//      const om = document.querySelectorAll('img');
+    
+//      setInterval(() => {
+    
+//         om.forEach((item,i)=>{
+//     for(let o = 0 ; o<=item.lenght; o++){
+
+//              i.classList.toggle('y');
+//     }
+
+    
+         
+   
+  
+
+// // })
+
+// //      },5000)
+// // 11
+
+// let arr =['https://bipbap.ru/wp-content/uploads/2017/04/7529_600.jpg',
+// 'https://cdn.getyourguide.com/img/tour_img-582875-148.jpg',
+// 'https://cdn.getyourguide.com/img/tour_img-582875-148.jpg'];
+
+// arr.forEach(io,i => {
+           
+    
+// });
+
+
+
+// let arr = [2,3],
+// o2={};
+
+// [o2.a, o2.b]=arr;
+// console.log(o2.b);
+
+// let newOp = { a:2 ,  t:'y' ,  u:34},
+// o3 = [];
+// ({a:o3[2], t:o3[1], u:o3[0]}=newOp);
+// console.log(o3[0],o3[2],o3[1])
+
+// let i = [ 2 , 3 , 4 , 5 , 6 , 8],
+// u = [];
+
+// [u[1],u[0],u[2],u[3],u[4]]=i;
+// console.log(u)
+// function f6({x=10}={},{y} = {y:10}){
+//     console.log(x,y)
+// }
+// f6({},{});
+// let p ={x:10,y:30},
+// pi=[];
+// ({x:pi[0],y:pi[1]}=p)
+// console.log(pi);
+
+// let i = [3,5,6,7,76],
+// iq={};
+// [iq[0],iq[1],iq[2]]=i;
+// console.log(iq)
+// {
+// let {
+//     op: {
+//         r = defaults.oq.r,
+//         e = defaults.oq.e
+//     } = {},
+//     log: {
+//         tit = defaults.rty.tit,
+//         tot= defaults.rty.tot
+//     }= {}
+// } = con;
+
+// con =  {
+//         oq:{r,e},
+//         rty: {tit,tot}
+       
+// };
+
+// console.log(op)
+
+// }
+// let i = 0;
+// while(i<100){
+//     console.log(i)
+    
+//     }
+
+// if(i%3==0){
+    //     console.log('fizz');
+// let u = 0;
+//    while(u<=7){
+//        u++;
+//        console.log('#')
+//    };
+// let o="";
+//    for(var i = 0;i<7;i++){
+//        o+='#';
+//       console.log(`${o}`);
+//    };
+
+
+// function prob (a,b){
+// if(a>b){
+//     return b;
+// }
+// else{
+//     return a;
+// }
+// };
+// console.log(prob(1,12));
+// console.log(prob(20,113));
+
+
+// function p2 (i){
+//     if(i%2==0){
+//         console.log('четное')
+//     }
+//     else{
+//         console.log('не')
+//     }
+// }
+// p2(50);
+// p2(75);
+// p2(-2);
+// p2(-51);
+
+// function p3(str,y){
+   
+//     for(let i = 0; i<str.length; i++){
+//         if(str[i]==y){
+//             console.log(y)
+//         }
+//     }
+// }
+// p3('vladislav', 'v');
+
+// var mass =[];
+// function ran(a,b,c=1){
+//  if(a<b){
+//     var p = b-a+1 ;
+// for(var pi = 0 ; pi<=p; pi+=c ){
+// let mass = mass[pi];
+// console.log(mass)
+// }
+// }
+// else if(a>b){
+//     var pr = a-b+1;
+
+// for(var pi = 0 ; pi<=pr; pi+=c ){
+//     console.log(pi);
+    
+     
+//     }
+// } 
+
+
+// }
+
+// function sum(a){
+//     return a + a;
+// }
+// console.log(ran(1,10));
+
+// let ceil = 0;
+// function look(a=1,b=15,c=1){
+//     let mass = [];
+//     for(let i = a; i<=b;i+=c){
+//    ceil += i;
+
+// mass.push(mass[i]);
+ 
+//     } 
+//     return mass;
+  
+// } 
+
+// look(1,10,1);
+
+ 
+//  var a = ["a", "b", "c"];
+// var index;
+// for (index = 0; index < a.length; ++index) {
+//     console.log(a[index].repeat());
+// }
+
+
+
+// let neg = [];
+//  for(let i = 0;i<10;i+=3){
+//      neg.push(i)
+//  }
+//  console.log(neg);
+// console.log(neg[2]);
+// neg.filter(item=>{
+//     return item <-1
+// })    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// f1();
+// function f2(){
+
+//  let btn = document.querySelector('.btn0').addEventListener('click',()=>{
+//      var ot = ott.value;
+//     console.log(ot)
+// })
+// }
+ 
+ 
+
+// let o = 0;
+// function f1 (){  
+//    let x1 = document.querySelector('.l2');
+// let y1 = document.querySelector('.l3');
+
+//     for(let i = 0 ; i<10;i++){
+//  let x =  Math.round( Math.random()*9);
+//   let y = Math.round( Math.random()*10);
+// x1.innerHTML = x;
+// y1.innerHTML = y;
+// console.log(x,y);
+// let p = x*y;
+// let ott  = document.querySelector('input');
+// if(p===ott.value){ 
+//       o++;
+//       console.log('правильно');
+     
+//       console.log(o)
+//       if(o === 5){
+//          console.log('5 раз подряд правильно');
+//          break;
+//       }
+//   }
+//   else{
+//       o = 0;
+//       console.log(o)
+//       console.log('попробуй ещё')
+//   }
+// }
+//  f2();
+// //  let ot = +prompt('ввиди сумму');
+// }
+
+ 
+
+
+
+
+
+
+
+
+
+   let ans = document.querySelector('.ping');
+    let input = document.querySelector('#in'),
+     btn = document.querySelector('.btn0');
+     let btn1 =document.querySelectorAll('.btn0');
+
+
+
+
+//Кнопка "начать"
+let b ;
+btn1[1].addEventListener('click',()=>{
+    b = ran();
+});
+//
+
+
+
+//кнопка "проверить"
+btn.addEventListener('click',()=>{
+    let z = Number(input.value);
+ iff(z,b);
+ clearInput();
+ });
+ //
+ function fff(){
+     while(true){
+        iff(z,b)
+     }
+ }
+
+//функция которая создает случайное число и присваивает x && y
+function ran ()
+{let x1 = document.querySelector('.l2'),
+y1 = document.querySelector('.l3');
+let x11 = Math.round(Math.random()*10),
+y11 = Math.round(Math.random()*10);
+ans.innerHTML = `Поехали  `
+x1.innerHTML = x11;
+y1.innerHTML = y11;
+ b = x11 * y11
+return b
+}
+// 
+ 
+ //Проверка ответа 
+function iff(z,t){
+ 
+    if(z === t){
+        ans.innerHTML = `<p style="color:green; margin-top:15px;">Молодец, Верно</p>`
+          let span = document.querySelector('.pingSpan');
+          span.addEventListener('click', ran)
+    }
+    else{
+        ans.innerHTML = `<p style="color:red; margin-top:15px">Попробуй ещё раз</p>`
+    }
+
+}
+//
+
+
+  function clearInput (){
+      const int = document.querySelector('input');
+      int.value = '';
+  }
+  
+   
+
+
+ 
+
+
+    
+
+
+
+
+ 
+
+
+ 
+
+
